776f9b41bcfa16863bebfbf94e8f3857f60a523f
Felipe Arceno <arceno65@gmail.com>
2023-02-20 15:31:47 -0300

    Part 9: Escutando Eventos

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -14,14 +14,32 @@ const Formulario = () => {
     "Minivan",
   ];
 
+  const aoSalvar = (event) => {
+    event.preventDefault();
+
+    console.log("Form foi submetido");
+  };
+
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do veículo</h2>
-        <CampoTexto label="Marca" placeholder="Digite a marca" />
-        <CampoTexto label="Modelo" placeholder="Digite o modelo" />
+        <CampoTexto
+          obrigatorio={true}
+          label="Marca"
+          placeholder="Digite a marca"
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Modelo"
+          placeholder="Digite o modelo"
+        />
         <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-        <ListaSuspensa label="Categoria" itens={categorias} />
+        <ListaSuspensa
+          obrigatorio={true}
+          label="Categoria"
+          itens={categorias}
+        />
         <Botao>Criar Card</Botao>
       </form>
     </section>
