014e1f07bd47e14370607040aeb56ec2d250cabd
Felipe Arceno <arceno65@gmail.com>
2023-02-20 15:00:13 -0300

    Part 7: Lista de Categorias

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,14 +1,26 @@
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
 const Formulario = () => {
+  const categorias = [
+    "Hatch",
+    "Sedan",
+    "SUV",
+    "Picape",
+    "Conversível",
+    "Station Wagon",
+    "Minivan",
+  ];
+
   return (
     <section className="formulario">
       <form>
-        <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto label="Nome" placeholder="Digite o seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite o seu cargo" />
+        <h2>Preencha os dados para criar o card do veículo</h2>
+        <CampoTexto label="Marca" placeholder="Digite o seu nome" />
+        <CampoTexto label="Modelo" placeholder="Digite o seu cargo" />
         <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+        <ListaSuspensa label="Categoria" itens={categorias} />
       </form>
     </section>
   );
